d1a444220c54b097496f02418fbbe070121e6c6d
kentntwari <kent.ntwari@ncrfs.com>
2024-01-12 16:45:13 -0500

    fix: give unique id o list items

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,7 +2,7 @@
   <main class="mx-5 my-8">
     <Invoices v-slot="{ invoices }">
       <ul class="mt-8 flex-1 flex flex-col gap-4">
-        <li v-for="invoice in invoices" :key="crypto.randomUUID()">
+        <li v-for="invoice in invoices" :key="randomKey()">
           <RouterLink to="/">
             <Invoice :invoice="invoice">
               <template #header="{ id, clientName }">
@@ -31,8 +31,11 @@
 </template>
 
 <script>
+import { v4 as uuidv4 } from "uuid";
+
 // components
 import { RouterLink } from "vue-router";
+import Status from "@/components/Status";
 import Invoices from "@/components/Invoices";
 import Invoice from "@/components/Invoices/Invoice";
 
@@ -42,6 +45,12 @@ export default {
     RouterLink,
     Invoices,
     Invoice,
+    Status,
+  },
+  methods: {
+    randomKey() {
+      return uuidv4();
+    },
   },
 };
 </script>
