4031f3fffd6400d2df701defeb33bc731ba74916
Kent Ntwari <designerjehovah@gmail.com>
2024-01-15 06:32:28 -0500

    feat: extend prisma

--- next/lib/prismaClient.js
+++ next/lib/prismaClient.js
@@ -2,4 +2,28 @@ import { PrismaClient } from "@prisma/client";
 
 export const prisma = global.prisma || new PrismaClient();
 
+export const extendedPrisma = prisma.$extends({
+  result: {
+    order: {
+      total: {
+        needs: { quantity: true, price: true },
+        compute(order) {
+          return order.quantity * order.price;
+        },
+      },
+    },
+    invoice: {
+      grandTotal: {
+        needs: { orders: true },
+        compute(invoice) {
+          return invoice.orders.reduce(
+            (accumulator, order) => accumulator + order.total,
+            0
+          );
+        },
+      },
+    },
+  },
+});
+
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
