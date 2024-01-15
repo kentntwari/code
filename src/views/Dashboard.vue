8a71320a1f3f0ab775d40872d6175bdcfeb2bdb9
Kent Ntwari <designerjehovah@gmail.com>
2024-01-15 06:24:55 -0500

    refactor: update logic

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,40 +1,41 @@
 <template>
-  <main class="mx-5 my-8">
-    <Invoices v-slot="{ invoices }">
-      <ul class="mt-8 flex-1 flex flex-col gap-4">
-        <li v-for="invoice in invoices" :key="randomKey()">
-          <RouterLink to="/">
-            <Invoice :invoice="invoice">
-              <template #header="{ id, clientName }">
-                <div class="font-bold text-SV">
-                  <span class="text-gray-secondary">#</span>
-                  <span>{{ id }}</span>
-                </div>
-                <small class="text-baseV text-gray-secondary">{{ clientName }}</small>
-              </template>
+  <Invoices v-slot="{ invoices }">
+    <ul class="mt-8 flex-1 flex flex-col gap-4">
+      <li v-for="invoice in invoices" :key="randomKey()">
+        <RouterLink :to="{ name: 'invoice', params: { id: invoice.id.toLowerCase() } }">
+          <Invoice
+            class="p-6 bg-white flex flex-col gap-6 rounded-lg shadow-sm"
+            :invoice="invoice">
+            <template #header="{ id, clientName }">
+              <div class="font-bold text-SV">
+                <span class="text-gray-secondary">#</span>
+                <span>{{ id }}</span>
+              </div>
+              <small class="text-baseV text-gray-secondary">{{ clientName }}</small>
+            </template>
 
-              <template #footer="{ due, total, status }">
-                <div class="flex flex-col gap-2">
-                  <small class="text-baseV text-gray-secondary">Due {{ due }}</small>
-                  <span class="text-S">£ {{ total }}</span>
-                </div>
-                <Status class="capitalize" :status="status">
-                  {{ invoice.status }}
-                </Status>
-              </template>
-            </Invoice>
-          </RouterLink>
-        </li>
-      </ul>
-    </Invoices>
-  </main>
+            <template #footer="{ dueDate, total, status }">
+              <div class="flex flex-col gap-2">
+                <small class="text-baseV text-gray-secondary">Due {{ dueDate }}</small>
+                <span class="text-S">£ {{ total }}</span>
+              </div>
+              <Status :status="status" />
+            </template>
+          </Invoice>
+        </RouterLink>
+      </li>
+    </ul>
+  </Invoices>
+
+  <!-- Nested new invoice -->
+  <RouterView />
 </template>
 
 <script>
 import { v4 as uuidv4 } from "uuid";
 
 // components
-import { RouterLink } from "vue-router";
+import { RouterLink, RouterView } from "vue-router";
 import Status from "@/components/Status";
 import Invoices from "@/components/Invoices";
 import Invoice from "@/components/Invoices/Invoice";
