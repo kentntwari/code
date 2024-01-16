0fa3c1496d84e5779373cc13ad170c39bfa030e5
Kent Ntwari <designerjehovah@gmail.com>
2024-01-16 08:33:40 -0500

    add plugins

---
+++ plugins/fetch.js
@@ -0,0 +1,19 @@
+import { ofetch } from "ofetch";
+
+export default {
+  install: (app, options) => {
+    app.config.globalProperties.$fetch = ofetch.create({
+      retry: 5,
+      retryDelay: 500,
+      timeout: 3000,
+      onRequestError: async ({ request, error }) => {
+        // Log error
+        console.log("[fetch request error]", request, error);
+      },
+      onResponseError: async ({ request, response }) => {
+        // Log error
+        console.log("[fetch response error]", request, response.status, response.body);
+      },
+    });
+  },
+};
