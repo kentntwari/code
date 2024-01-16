560f2efa6d8a7c79ad00ba2fd0ad8d24f4501300
Kent Ntwari <designerjehovah@gmail.com>
2024-01-16 08:35:55 -0500

    feat:register plugin

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import "./main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import fetch from "../plugins/fetch";
+
 const app = createApp(App).use(router);
 
 app.mount("#app");
+app.use(fetch);
