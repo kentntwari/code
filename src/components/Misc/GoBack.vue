9f84f9227acda482766f871e7915f38a59ee46ae
Kent Ntwari <designerjehovah@gmail.com>
2024-01-18 07:27:38 -0500

    feat: init Miscelleanoeus components

---
+++ src/components/Misc/GoBack.vue
@@ -0,0 +1,23 @@
+<template>
+  <button
+    type="button"
+    title="Go back"
+    class="flex items-center gap-6 cursor-pointer"
+    @click="$router.go(-1)">
+    <ArrowLeftSVG />
+    <h1 class="font-bold text-heading-S text-gray-primary">Go back</h1>
+  </button>
+</template>
+
+<script>
+import ArrowLeftSVG from "@/components/Svg/ArrowLeft";
+
+export default {
+  name: "GoBackBtn",
+  components: {
+    ArrowLeftSVG,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
