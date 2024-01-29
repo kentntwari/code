be9af0c111c83b30572dee0d87b283ccfdf57791
Kent Ntwari <designerjehovah@gmail.com>
2024-01-29 13:14:51 -0500

    feat: add post request

--- next/src/pages/api/invoices/[id].js
+++ next/src/pages/api/invoices/[id].js
@@ -51,11 +51,115 @@ export default async function handler(req, res) {
       }
     }
 
+    case "POST": {
+      try {
+        console.log(req.body);
+
+        await extendedPrisma.$transaction(async (tx) => {
+          const client = await tx.client.upsert({
+            where: {
+              client_specs: {
+                name: req.body.client.name,
+                email: req.body.client.email,
+                street: req.body.client.street,
+                city: req.body.client.city,
+                postCode: req.body.client.postCode,
+                country: req.body.client.country,
+              },
+            },
+            update: {},
+            create: {
+              name: req.body.client.name,
+              email: req.body.client.email,
+              street: req.body.client.street,
+              city: req.body.client.city,
+              postCode: req.body.client.postCode,
+              country: req.body.client.country,
+            },
+          });
+
+          const sender = await tx.sender.upsert({
+            where: {
+              street: req.body.sender.street,
+              city: req.body.sender.city,
+              postCode: req.body.sender.postCode,
+              country: req.body.sender.country,
+            },
+            update: {},
+            create: {
+              street: req.body.sender.street,
+              city: req.body.sender.city,
+              postCode: req.body.sender.postCode,
+              country: req.body.sender.country,
+            },
+          });
+
+          const invoice = await tx.invoice.create({
+            data: {
+              id: req.body.invoiceID.toUpperCase(),
+              status: "pending",
+              description: req.body.invoice.description,
+              paymentTerms: !req.body.invoice.paymentTerms
+                ? 1
+                : req.body.invoice.paymentTerms,
+              dueDate: new Date(req.body.invoice.dueDate),
+              client: {
+                connect: {
+                  id: client.id,
+                },
+              },
+              orders: {
+                createMany: {
+                  data: req.body.orders.map((order) => ({
+                    ...order,
+                    senderId: sender.id,
+                  })),
+                },
+              },
+            },
+          });
+
+          const curateOrdersByInvoice = await tx.order.findMany({
+            where: {
+              invoiceId: invoice.id,
+            },
+          });
+
+          return await tx.client.update({
+            where: {
+              id: client.id,
+            },
+            data: {
+              orders: {
+                connect: curateOrdersByInvoice.map((order) => ({
+                  id: order.id,
+                })),
+              },
+            },
+          });
+        });
+
+        res.status(201).json({ message: "Invoice created" });
+        extendedPrisma.$disconnect();
+        return;
+      } catch (error) {
+        res.status(500).json({ message: "Something went wrong", error });
+        extendedPrisma.$disconnect();
+        return;
+      }
+    }
+
+    case "PUT": {
+      console.log(req.body);
+      res.end();
+      return;
+    }
+
     case "PATCH": {
       const incomingHeaders = new Headers(req.headers);
 
       if (incomingHeaders.get("X-patch-request-action") !== "mark-as-paid") {
-        res.status(400).json({ message: "Invalid request action" });
+        res.status(400).json({ message: "Invalid request actiondcdscsdcdsc" });
         return;
       }
 
