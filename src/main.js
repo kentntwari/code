464444bae040acde04882d9b56bf262e8d6b85fb
Kent Ntwari <designerjehovah@gmail.com>
2024-01-31 06:40:12 -0500

    feat: register globally radix alerts components

--- src/main.js
+++ src/main.js
@@ -4,9 +4,30 @@ import "./main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import {
+  AlertDialogRoot,
+  AlertDialogTrigger,
+  AlertDialogPortal,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogTitle,
+  AlertDialogAction,
+  AlertDialogDescription,
+} from "radix-vue";
+
 import fetch from "../plugins/fetch";
 
 const app = createApp(App).use(router);
 
 app.mount("#app");
+
 app.use(fetch);
+
+app.component("RAlertRoot", AlertDialogRoot);
+app.component("RAlertTrigger", AlertDialogTrigger);
+app.component("RAlertPortal", AlertDialogPortal);
+app.component("RAlertOverlay", AlertDialogOverlay);
+app.component("RAlertContent", AlertDialogContent);
+app.component("RAlertTitle", AlertDialogTitle);
+app.component("RAlertAction", AlertDialogAction);
+app.component("RAlertDescription", AlertDialogDescription);
