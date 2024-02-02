5cabd95e8f485ae8a3dd2c2aaf7d20be72e93df4
Kent Ntwari <designerjehovah@gmail.com>
2024-02-02 06:36:23 -0500

    update navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
-  <nav class="flex items-center justify-between">
-    <div class="uppercase text-heading-S">Logo</div>
-    <span>Toggle</span>
+  <nav
+    class="relative z-20 overflow-clip h-18 bg-slate-primary flex items-center justify-between">
+    <div
+      class="relative w-18 h-18 bg-violet-primary flex items-center justify-center rounded-tr-[20px] rounded-br-[20px] before:absolute before:w-18 before:h-18 before:bg-violet-secondary before:top-9 before:rounded-tl-2xl before:rounded-bl-[20px]">
+      <LogoSVG class="relative z-10" />
+    </div>
+
+    <div class="h-full grid grid-cols-2">
+      <div class="w-full h-full flex items-center justify-center"><MoonSVG /></div>
+      <div
+        class="w-20 h-full flex items-center justify-center border-l border-gray-secondary/40">
+        <img
+          class="w-8 h-8 object-cover rounded-full"
+          src="/src/assets/image-avatar.jpg" />
+      </div>
+    </div>
   </nav>
   <main class="relative flex-1 px-5 py-8">
     <router-view></router-view>
@@ -9,8 +22,15 @@
 </template>
 
 <script>
+import LogoSVG from "@/components/Svg/Logo";
+import MoonSVG from "@/components/Svg/Moon";
+
 export default {
   name: "App",
+  components: {
+    LogoSVG,
+    MoonSVG,
+  },
 };
 </script>
 
