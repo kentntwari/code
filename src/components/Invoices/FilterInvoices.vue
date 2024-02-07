d5a2c1fe29475f2c37ce50c41e316030bb77a457
Kent Ntwari <designerjehovah@gmail.com>
2024-02-07 04:43:31 -0500

    feat: persist and handle state in url with queryString

--- src/components/Invoices/FilterInvoices.vue
+++ src/components/Invoices/FilterInvoices.vue
@@ -2,23 +2,25 @@
   <CollapsibleRoot v-model:open="open" class="relative m-auto">
     <CollapsibleTrigger class="flex items-center gap-3">
       <span class="text-SV text-black-site"
-        >Filter <ins class="hidden no-underline">by status</ins></span
+        >Filter <ins class="hidden md:inline-block no-underline">by status</ins></span
       >
       <ArrowDownSVG />
     </CollapsibleTrigger>
     <CollapsibleContent
       class="absolute top-6 left-0 -translate-x-1/4 data-[state=open]:p-6 data-[state=open]:w-[192px] bg-white flex flex-col gap-4 shadow-md rounded-lg">
       <div v-for="status in arr" :key="status.text">
-        <CheckboxRoot v-model:checked="status.value" class="flex items-center gap-3">
+        <CheckboxRoot
+          v-model:checked="status.value"
+          class="flex items-center gap-3"
+          @click="filter">
           <span
-            class="w-4 h-4 rounded-[2px]"
-            :class="[!status.value ? 'bg-violet-secondary/20' : 'bg-violet-primary']"
-           >
+            class="w-4 h-4 flex items-center justify-center rounded-[2px]"
+            :class="[!status.value ? 'bg-violet-secondary/20' : 'bg-violet-primary']">
             <CheckboxIndicator @click="status.value = !status.value">
-              <ArrowLeftSVG />
+              <CheckSVG />
             </CheckboxIndicator>
           </span>
-          <span class="text-SV">
+          <span class="text-SV capitalize">
             {{ status.text }}
           </span>
         </CheckboxRoot>
@@ -28,7 +30,9 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, nextTick } from "vue";
+import queryString from "query-string";
+import CheckSVG from "@/components/Svg/Check.vue";
 import ArrowDownSVG from "@/components/Svg/ArrowDown";
 import ArrowLeftSVG from "@/components/Svg/ArrowLeft";
 import { CheckboxIndicator, CheckboxRoot } from "radix-vue";
@@ -42,6 +46,7 @@ export default {
     CollapsibleRoot,
     CollapsibleTrigger,
     CollapsibleContent,
+    CheckSVG,
     ArrowDownSVG,
     ArrowLeftSVG,
   },
@@ -49,11 +54,29 @@ export default {
     return {
       open: false,
       arr: [
-        { text: "Draft", value: false },
-        { text: "Pending", value: false },
-        { text: "Paid", value: false },
+        { text: "draft", value: false },
+        { text: "pending", value: false },
+        { text: "paid", value: false },
       ],
     };
+  },
+  mounted() {
+    const parsed = queryString.parse(window.location.search, { arrayFormat: "bracket" });
+    this.arr = this.arr.map((item) => {
+      if (parsed?.filter?.includes(item.text)) return { ...item, value: true };
+      return { ...item };
+    });
+  },
+  methods: {
+    async filter() {
+      await nextTick();
+
+      const selected = this.arr
+        .filter(({ value }) => value === true)
+        .map(({ text }) => text.toLowerCase());
+
+      this.$router.push({ query: { ...this.$router.query, "filter[]": [...selected] } });
+    },
   },
 };
 </script>
